a0eda28689059b4a5e1b988e5917b2ebaa6cb84f
SethWickham <seth.wickham@hotmail.com>
2019-06-25 13:05:54 -0700

    added correction to comments

--- database/database.js
+++ database/database.js
@@ -1,5 +1,5 @@
-// using the built in Node function require() we are
-// able to load our modules. This helps us split our application
+// using the built in Node method require() we are
+// able to load our modules. Loading modules helps us to split our application
 // up into files and thus organize our code more neatly.
 //here we are loading in the module mongoose and config.
 // const db is using a get method to bring in mongoURI from mongo.json
@@ -8,8 +8,14 @@ const mongoose = require('mongoose');
 const config = require('config');
 const database = config.get('mongoURI');
 
-//creating an arrow function so that we can
+//creating an arrow function which will run the code that connects our database
+// or will throw an error and exit the program.
 const dbConnector = async () => {
+  // using try lets us test the code block for errors
+  // here we are then console logging the error with the attached message
+  // that it would recieve. We use await to wait for the connection to our database being passed
+  // in from above where we have set it to mongoURI
+  //
   try {
     await mongoose.connect(database);
   } catch (error) {

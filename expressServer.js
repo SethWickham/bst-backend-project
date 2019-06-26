b4060db3f8c077f7aa26f591e2b2f3a0f3484129
SethWickham <seth.wickham@hotmail.com>
2019-06-26 16:50:00 -0700

    created .env file storing database connection

--- expressServer.js
+++ expressServer.js
@@ -1,16 +1,17 @@
 // here we are using built in require method to bring in our installed dependencies
 // which will help us to run our application
 
-//  bringing in express to use in our application
+//  bringing in express to use in our application since we are going to be creating
+// an express backend
 const express = require('express');
 // we are creating an express application and setting this as our backEnd
 const backend = express();
-// bringing in cors which will allow our server to take in requests and communicate to a different domain
+// bringing in cors dependency which will allow our server to take in requests and communicate to a different domain
 const cors = require('cors');
-
+// bringing in mongoose dependency which helps us to connect to our mongodb database
 const mongoose = require('mongoose');
 
-//bringing in dotenv which is how we will store our connection to our database into a .env file
+//bringing in dotenv dependecy which is how we will store our connection to our database into a .env file
 require('dotenv').config();
 
 //This is how our API routes interface with the expressServer.js file
@@ -23,7 +24,16 @@ backend.use('/API/firstNames', require('./routes/API/firstNames'));
 //this allows us to parse json so that we can send and recieve json into the database
 backend.use(express.json);
 
+//the flag for useNewUrlParser and useCreateIndex is updated syntax so that it will run properly in the future
+const URIdatabase = process.env.ATLAS_URI_DB;
+mongoose.connect(URIdatabase, { useNewUrlParser: true, useCreateIndex: true });
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('Mongo Database Connected Good Job!');
+});
 //here we are setting up our port to run on 3200 and using our dotenv which we brought in above
+// side note I LOVE 32 ...long story
 const PORTENTRY = process.env.PORT || 3200;
 
 //using the built in listen() method we are passing in our PORTENTRY value
